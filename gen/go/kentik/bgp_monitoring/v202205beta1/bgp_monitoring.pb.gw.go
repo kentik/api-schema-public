bbe650039626aa6247bd199f17fba38ff1056fab
Martin Machacek <martin+github@kentik.com>
2022-05-12 11:13:48 -0700

    Removing RPC for retrieving list of BGP events, because the backend cannot currently reliably provide the data. (#133)
    GitOrigin-RevId: 44b9e535933b213640795a5d992a58f6b564809c

--- gen/go/kentik/bgp_monitoring/v202205beta1/bgp_monitoring.pb.gw.go
+++ gen/go/kentik/bgp_monitoring/v202205beta1/bgp_monitoring.pb.gw.go
@@ -323,40 +323,6 @@ func local_request_BgpMonitoringAdminService_SetMonitorStatus_0(ctx context.Cont
 
 }
 
-func request_BgpMonitoringDataService_GetEventsForMonitor_0(ctx context.Context, marshaler runtime.Marshaler, client BgpMonitoringDataServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq GetEventsForMonitorRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.GetEventsForMonitor(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func local_request_BgpMonitoringDataService_GetEventsForMonitor_0(ctx context.Context, marshaler runtime.Marshaler, server BgpMonitoringDataServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq GetEventsForMonitorRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := server.GetEventsForMonitor(ctx, &protoReq)
-	return msg, metadata, err
-
-}
-
 func request_BgpMonitoringDataService_GetMetricsForMonitor_0(ctx context.Context, marshaler runtime.Marshaler, client BgpMonitoringDataServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetMetricsForMonitorRequest
 	var metadata runtime.ServerMetadata
@@ -577,29 +543,6 @@ func RegisterBgpMonitoringAdminServiceHandlerServer(ctx context.Context, mux *ru
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterBgpMonitoringDataServiceHandlerFromEndpoint instead.
 func RegisterBgpMonitoringDataServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server BgpMonitoringDataServiceServer) error {
-
-	mux.Handle("POST", pattern_BgpMonitoringDataService_GetEventsForMonitor_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/kentik.bgp_monitoring.v202205beta1.BgpMonitoringDataService/GetEventsForMonitor")
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_BgpMonitoringDataService_GetEventsForMonitor_0(rctx, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_BgpMonitoringDataService_GetEventsForMonitor_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
 
 	mux.Handle("POST", pattern_BgpMonitoringDataService_GetMetricsForMonitor_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
@@ -877,26 +820,6 @@ func RegisterBgpMonitoringDataServiceHandler(ctx context.Context, mux *runtime.S
 // "BgpMonitoringDataServiceClient" to call the correct interceptors.
 func RegisterBgpMonitoringDataServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client BgpMonitoringDataServiceClient) error {
 
-	mux.Handle("POST", pattern_BgpMonitoringDataService_GetEventsForMonitor_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/kentik.bgp_monitoring.v202205beta1.BgpMonitoringDataService/GetEventsForMonitor")
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_BgpMonitoringDataService_GetEventsForMonitor_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_BgpMonitoringDataService_GetEventsForMonitor_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("POST", pattern_BgpMonitoringDataService_GetMetricsForMonitor_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -941,16 +864,12 @@ func RegisterBgpMonitoringDataServiceHandlerClient(ctx context.Context, mux *run
 }
 
 var (
-	pattern_BgpMonitoringDataService_GetEventsForMonitor_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"bgp_monitoring", "v202205beta1", "events"}, ""))
-
 	pattern_BgpMonitoringDataService_GetMetricsForMonitor_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"bgp_monitoring", "v202205beta1", "metrics"}, ""))
 
 	pattern_BgpMonitoringDataService_GetRoutesForMonitor_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"bgp_monitoring", "v202205beta1", "routes"}, ""))
 )
 
 var (
-	forward_BgpMonitoringDataService_GetEventsForMonitor_0 = runtime.ForwardResponseMessage
-
 	forward_BgpMonitoringDataService_GetMetricsForMonitor_0 = runtime.ForwardResponseMessage
 
 	forward_BgpMonitoringDataService_GetRoutesForMonitor_0 = runtime.ForwardResponseMessage
