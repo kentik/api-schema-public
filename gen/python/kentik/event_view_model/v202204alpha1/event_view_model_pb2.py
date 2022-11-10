b0536656811b6aba17cbde9436f6481d3ba4e9f4
Machine User <50119483+kntkmachinery@users.noreply.github.com>
2022-11-09 22:37:23 -0800

    Apply api-schema changes (#9)
    * Update notification_channel.proto (#201)
    * Simplify description of the `SearchNotificationChannels` RPC in notification_channel.proto
    
    GitOrigin-RevId: 7ea2585ade04ec871896c5ef0a6d0ed30570931a
    
    * Fixup `info.version` strings of all API GA versions to contain the leading `v` (#202)
    
    GitOrigin-RevId: 31a797bdb489fac44f46a3ea3d6ccd4f5d7bb5b3
    
    * Support mitigation and insight event within EVM (#203)
    
    GitOrigin-RevId: 043aa27fc4eeadaf4a6c620ac61e83b3dbb147f6
    
    * Enrich DB insight definitions with dimensions and metrics (#200)
    
    * Add rawDimensionToDimension, rawMetricToMetric to DBInsightDefinition
    
    The "DB" insight definition type is used for queries against chwww
    (and potentially other databases in the future). However, it doesn't
    currently define a way to specify which columns in the executed query
    correspond to particular dimensions or metrics.
    
    For the `security.device.firmware` insight driving the development of
    this feature, I need at least the `device_id` dimension. Metric is less
    obviously justifiable, but it's cheaper to add it and not need it than
    to miss it out.
    
    This approach is copied from the `KSQLInsightDefinition`, where it
    works well. `KDEInsightDefinition` just uses `dimensions` and `metrics`
    but it's useful to be able to rename the SQL results.
    
    GitOrigin-RevId: b7d88398b7a5b5acf9a7e8ac847bb8de104403d2
    
    * Fix a bug in the insights meta proto (#204)
    
    GitOrigin-RevId: 2bea8ae9ef19e55edc75c3392d1b34927ba92e6e
    
    * GA version of site API (#205)
    Co-authored-by: pdelancie <pdelancie@cloudhelix.com>
    GitOrigin-RevId: 855b9ff5ef95c94b251c81db4f6deb96c52076a5
    
    * Add boolean status/hidden fields in InsightDefinition (#206)
    
    GitOrigin-RevId: fa1efe42a4a997467907aa333e6a286d73b5a179
    
    * GA version of public User API (#208)
    
    Co-authored-by: pdelancie <pdelancie@cloudhelix.com>
    Co-authored-by: Karol Lassak <ingwar@kentik.com>
    GitOrigin-RevId: 5f145b591b0e30da4c5833ba6430f1b45c7b8190
    
    Co-authored-by: pdelancie <pdelancie@cloudhelix.com>
    Co-authored-by: Martin Machacek <martin+github@kentik.com>
    Co-authored-by: Remigiusz Orłowski <remi00@users.noreply.github.com>
    Co-authored-by: Nick Thomas <nthomas@kentik.com>
    Co-authored-by: Filippos Giannakos <f.giannakos@gmail.com>
    Co-authored-by: Karol Lassak <ingwar@kentik.com>

--- gen/python/kentik/event_view_model/v202204alpha1/event_view_model_pb2.py
+++ gen/python/kentik/event_view_model/v202204alpha1/event_view_model_pb2.py
@@ -21,7 +21,7 @@ from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__
 from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n<kentik/event_view_model/v202204alpha1/event_view_model.proto\x12%kentik.event_view_model.v202204alpha1\x1a\x1cgoogle/api/annotations.proto\x1a\x17google/api/client.proto\x1a.protoc-gen-openapiv2/options/annotations.proto\x1a+kentik/core/v202012alpha1/annotations.proto\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1cgoogle/protobuf/struct.proto\"\x1c\n\nAlarmEvent\x12\x0e\n\x02id\x18\x01 \x01(\tR\x02id\"i\n\x13GetViewModelRequest\x12I\n\x05\x61larm\x18\x02 \x01(\x0b\x32\x31.kentik.event_view_model.v202204alpha1.AlarmEventH\x00R\x05\x61larmB\x07\n\x05\x65vent\"{\n\x0f\x45ventViewDetail\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name\x12\x14\n\x05label\x18\x02 \x01(\tR\x05label\x12\x10\n\x03tag\x18\x03 \x01(\tR\x03tag\x12,\n\x05value\x18\x04 \x01(\x0b\x32\x16.google.protobuf.ValueR\x05value\"\x9b\x04\n\x0e\x45ventViewModel\x12H\n\x04type\x18\x01 \x01(\x0e\x32\x34.kentik.event_view_model.v202204alpha1.EventViewTypeR\x04type\x12\x14\n\x05group\x18\x02 \x01(\tR\x05group\x12 \n\x0b\x64\x65scription\x18\x03 \x01(\tR\x0b\x64\x65scription\x12\x1b\n\tis_active\x18\x04 \x01(\x08R\x08isActive\x12\x39\n\nstart_time\x18\x05 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\tstartTime\x12\x35\n\x08\x65nd_time\x18\x06 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\x07\x65ndTime\x12#\n\rcurrent_state\x18\x07 \x01(\tR\x0c\x63urrentState\x12%\n\x0eprevious_state\x18\x08 \x01(\tR\rpreviousState\x12Z\n\nimportance\x18\t \x01(\x0e\x32:.kentik.event_view_model.v202204alpha1.EventViewImportanceR\nimportance\x12P\n\x07\x64\x65tails\x18\n \x03(\x0b\x32\x36.kentik.event_view_model.v202204alpha1.EventViewDetailR\x07\x64\x65tails\"c\n\x14GetViewModelResponse\x12K\n\x05model\x18\x01 \x01(\x0b\x32\x35.kentik.event_view_model.v202204alpha1.EventViewModelR\x05model*\xcb\x01\n\rEventViewType\x12\x1f\n\x1b\x45VENT_VIEW_TYPE_UNSPECIFIED\x10\x00\x12\x19\n\x15\x45VENT_VIEW_TYPE_ALERT\x10\x01\x12\x1e\n\x1a\x45VENT_VIEW_TYPE_MITIGATION\x10\x02\x12\x1d\n\x19\x45VENT_VIEW_TYPE_SYNTHETIC\x10\x03\x12\x1b\n\x17\x45VENT_VIEW_TYPE_INSIGHT\x10\x04\x12\"\n\x1e\x45VENT_VIEW_TYPE_CUSTOM_INSIGHT\x10\x05*\xac\x02\n\x13\x45ventViewImportance\x12%\n!EVENT_VIEW_IMPORTANCE_UNSPECIFIED\x10\x00\x12!\n\x1d\x45VENT_VIEW_IMPORTANCE_HEALTHY\x10\x01\x12 \n\x1c\x45VENT_VIEW_IMPORTANCE_NOTICE\x10\x02\x12\x1f\n\x1b\x45VENT_VIEW_IMPORTANCE_MINOR\x10\x03\x12!\n\x1d\x45VENT_VIEW_IMPORTANCE_WARNING\x10\x04\x12\x1f\n\x1b\x45VENT_VIEW_IMPORTANCE_MAJOR\x10\x05\x12 \n\x1c\x45VENT_VIEW_IMPORTANCE_SEVERE\x10\x06\x12\"\n\x1e\x45VENT_VIEW_IMPORTANCE_CRITICAL\x10\x07\x32\x97\x03\n\x15\x45ventViewModelService\x12\xe1\x02\n\x0cGetViewModel\x12:.kentik.event_view_model.v202204alpha1.GetViewModelRequest\x1a;.kentik.event_view_model.v202204alpha1.GetViewModelResponse\"\xd7\x01\x92\x41\x98\x01\x12MEvent view model will provide event context necessary to render notifications\x1a\x39GetViewModel will provide event view model data structure*\x0cGetViewModel\x82\xd3\xe4\x93\x02\x35\"0/event_view_model/v202204alpha1/event_view_model:\x01*\x1a\x1a\xca\x41\x13grpc.api.kentik.com\xf8\xd7\x02\x01\x42\xfc\x01ZZgithub.com/kentik/api-schema/gen/go/kentik/event_view_model/v202204alpha1;event_view_model\x92\x41\x9c\x01\x12<\n\x12\x45ventViewModel API\"\x18\n\x16Kentik API Engineering2\x0c\x32\x30\x32\x32\x30\x34\x61lpha1*\x01\x02\x32\x10\x61pplication/json:\x10\x61pplication/jsonr5\n\x16More about Kentik APIs\x12\x1bhttps://docs.kentik.com/apib\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n<kentik/event_view_model/v202204alpha1/event_view_model.proto\x12%kentik.event_view_model.v202204alpha1\x1a\x1cgoogle/api/annotations.proto\x1a\x17google/api/client.proto\x1a.protoc-gen-openapiv2/options/annotations.proto\x1a+kentik/core/v202012alpha1/annotations.proto\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1cgoogle/protobuf/struct.proto\"\x1c\n\nAlarmEvent\x12\x0e\n\x02id\x18\x01 \x01(\tR\x02id\"!\n\x0fMitigationEvent\x12\x0e\n\x02id\x18\x01 \x01(\tR\x02id\"\x1e\n\x0cInsightEvent\x12\x0e\n\x02id\x18\x01 \x01(\tR\x02id\"\x94\x02\n\x13GetViewModelRequest\x12I\n\x05\x61larm\x18\x02 \x01(\x0b\x32\x31.kentik.event_view_model.v202204alpha1.AlarmEventH\x00R\x05\x61larm\x12X\n\nmitigation\x18\x03 \x01(\x0b\x32\x36.kentik.event_view_model.v202204alpha1.MitigationEventH\x00R\nmitigation\x12O\n\x07insight\x18\x04 \x01(\x0b\x32\x33.kentik.event_view_model.v202204alpha1.InsightEventH\x00R\x07insightB\x07\n\x05\x65vent\"{\n\x0f\x45ventViewDetail\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name\x12\x14\n\x05label\x18\x02 \x01(\tR\x05label\x12\x10\n\x03tag\x18\x03 \x01(\tR\x03tag\x12,\n\x05value\x18\x04 \x01(\x0b\x32\x16.google.protobuf.ValueR\x05value\"\x9b\x04\n\x0e\x45ventViewModel\x12H\n\x04type\x18\x01 \x01(\x0e\x32\x34.kentik.event_view_model.v202204alpha1.EventViewTypeR\x04type\x12\x14\n\x05group\x18\x02 \x01(\tR\x05group\x12 \n\x0b\x64\x65scription\x18\x03 \x01(\tR\x0b\x64\x65scription\x12\x1b\n\tis_active\x18\x04 \x01(\x08R\x08isActive\x12\x39\n\nstart_time\x18\x05 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\tstartTime\x12\x35\n\x08\x65nd_time\x18\x06 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\x07\x65ndTime\x12#\n\rcurrent_state\x18\x07 \x01(\tR\x0c\x63urrentState\x12%\n\x0eprevious_state\x18\x08 \x01(\tR\rpreviousState\x12Z\n\nimportance\x18\t \x01(\x0e\x32:.kentik.event_view_model.v202204alpha1.EventViewImportanceR\nimportance\x12P\n\x07\x64\x65tails\x18\n \x03(\x0b\x32\x36.kentik.event_view_model.v202204alpha1.EventViewDetailR\x07\x64\x65tails\"c\n\x14GetViewModelResponse\x12K\n\x05model\x18\x01 \x01(\x0b\x32\x35.kentik.event_view_model.v202204alpha1.EventViewModelR\x05model*\xcb\x01\n\rEventViewType\x12\x1f\n\x1b\x45VENT_VIEW_TYPE_UNSPECIFIED\x10\x00\x12\x19\n\x15\x45VENT_VIEW_TYPE_ALERT\x10\x01\x12\x1e\n\x1a\x45VENT_VIEW_TYPE_MITIGATION\x10\x02\x12\x1d\n\x19\x45VENT_VIEW_TYPE_SYNTHETIC\x10\x03\x12\x1b\n\x17\x45VENT_VIEW_TYPE_INSIGHT\x10\x04\x12\"\n\x1e\x45VENT_VIEW_TYPE_CUSTOM_INSIGHT\x10\x05*\xac\x02\n\x13\x45ventViewImportance\x12%\n!EVENT_VIEW_IMPORTANCE_UNSPECIFIED\x10\x00\x12!\n\x1d\x45VENT_VIEW_IMPORTANCE_HEALTHY\x10\x01\x12 \n\x1c\x45VENT_VIEW_IMPORTANCE_NOTICE\x10\x02\x12\x1f\n\x1b\x45VENT_VIEW_IMPORTANCE_MINOR\x10\x03\x12!\n\x1d\x45VENT_VIEW_IMPORTANCE_WARNING\x10\x04\x12\x1f\n\x1b\x45VENT_VIEW_IMPORTANCE_MAJOR\x10\x05\x12 \n\x1c\x45VENT_VIEW_IMPORTANCE_SEVERE\x10\x06\x12\"\n\x1e\x45VENT_VIEW_IMPORTANCE_CRITICAL\x10\x07\x32\x97\x03\n\x15\x45ventViewModelService\x12\xe1\x02\n\x0cGetViewModel\x12:.kentik.event_view_model.v202204alpha1.GetViewModelRequest\x1a;.kentik.event_view_model.v202204alpha1.GetViewModelResponse\"\xd7\x01\x92\x41\x98\x01\x12MEvent view model will provide event context necessary to render notifications\x1a\x39GetViewModel will provide event view model data structure*\x0cGetViewModel\x82\xd3\xe4\x93\x02\x35\"0/event_view_model/v202204alpha1/event_view_model:\x01*\x1a\x1a\xca\x41\x13grpc.api.kentik.com\xf8\xd7\x02\x01\x42\xfc\x01ZZgithub.com/kentik/api-schema/gen/go/kentik/event_view_model/v202204alpha1;event_view_model\x92\x41\x9c\x01\x12<\n\x12\x45ventViewModel API\"\x18\n\x16Kentik API Engineering2\x0c\x32\x30\x32\x32\x30\x34\x61lpha1*\x01\x02\x32\x10\x61pplication/json:\x10\x61pplication/jsonr5\n\x16More about Kentik APIs\x12\x1bhttps://docs.kentik.com/apib\x06proto3')
 
 _EVENTVIEWTYPE = DESCRIPTOR.enum_types_by_name['EventViewType']
 EventViewType = enum_type_wrapper.EnumTypeWrapper(_EVENTVIEWTYPE)
@@ -44,6 +44,8 @@ EVENT_VIEW_IMPORTANCE_CRITICAL = 7
 
 
 _ALARMEVENT = DESCRIPTOR.message_types_by_name['AlarmEvent']
+_MITIGATIONEVENT = DESCRIPTOR.message_types_by_name['MitigationEvent']
+_INSIGHTEVENT = DESCRIPTOR.message_types_by_name['InsightEvent']
 _GETVIEWMODELREQUEST = DESCRIPTOR.message_types_by_name['GetViewModelRequest']
 _EVENTVIEWDETAIL = DESCRIPTOR.message_types_by_name['EventViewDetail']
 _EVENTVIEWMODEL = DESCRIPTOR.message_types_by_name['EventViewModel']
@@ -54,6 +56,20 @@ AlarmEvent = _reflection.GeneratedProtocolMessageType('AlarmEvent', (_message.Me
   # @@protoc_insertion_point(class_scope:kentik.event_view_model.v202204alpha1.AlarmEvent)
   })
 _sym_db.RegisterMessage(AlarmEvent)
+
+MitigationEvent = _reflection.GeneratedProtocolMessageType('MitigationEvent', (_message.Message,), {
+  'DESCRIPTOR' : _MITIGATIONEVENT,
+  '__module__' : 'kentik.event_view_model.v202204alpha1.event_view_model_pb2'
+  # @@protoc_insertion_point(class_scope:kentik.event_view_model.v202204alpha1.MitigationEvent)
+  })
+_sym_db.RegisterMessage(MitigationEvent)
+
+InsightEvent = _reflection.GeneratedProtocolMessageType('InsightEvent', (_message.Message,), {
+  'DESCRIPTOR' : _INSIGHTEVENT,
+  '__module__' : 'kentik.event_view_model.v202204alpha1.event_view_model_pb2'
+  # @@protoc_insertion_point(class_scope:kentik.event_view_model.v202204alpha1.InsightEvent)
+  })
+_sym_db.RegisterMessage(InsightEvent)
 
 GetViewModelRequest = _reflection.GeneratedProtocolMessageType('GetViewModelRequest', (_message.Message,), {
   'DESCRIPTOR' : _GETVIEWMODELREQUEST,
@@ -92,20 +108,24 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _EVENTVIEWMODELSERVICE._serialized_options = b'\312A\023grpc.api.kentik.com\370\327\002\001'
   _EVENTVIEWMODELSERVICE.methods_by_name['GetViewModel']._options = None
   _EVENTVIEWMODELSERVICE.methods_by_name['GetViewModel']._serialized_options = b'\222A\230\001\022MEvent view model will provide event context necessary to render notifications\0329GetViewModel will provide event view model data structure*\014GetViewModel\202\323\344\223\0025\"0/event_view_model/v202204alpha1/event_view_model:\001*'
-  _EVENTVIEWTYPE._serialized_start=1220
-  _EVENTVIEWTYPE._serialized_end=1423
-  _EVENTVIEWIMPORTANCE._serialized_start=1426
-  _EVENTVIEWIMPORTANCE._serialized_end=1726
+  _EVENTVIEWTYPE._serialized_start=1459
+  _EVENTVIEWTYPE._serialized_end=1662
+  _EVENTVIEWIMPORTANCE._serialized_start=1665
+  _EVENTVIEWIMPORTANCE._serialized_end=1965
   _ALARMEVENT._serialized_start=314
   _ALARMEVENT._serialized_end=342
-  _GETVIEWMODELREQUEST._serialized_start=344
-  _GETVIEWMODELREQUEST._serialized_end=449
-  _EVENTVIEWDETAIL._serialized_start=451
-  _EVENTVIEWDETAIL._serialized_end=574
-  _EVENTVIEWMODEL._serialized_start=577
-  _EVENTVIEWMODEL._serialized_end=1116
-  _GETVIEWMODELRESPONSE._serialized_start=1118
-  _GETVIEWMODELRESPONSE._serialized_end=1217
-  _EVENTVIEWMODELSERVICE._serialized_start=1729
-  _EVENTVIEWMODELSERVICE._serialized_end=2136
+  _MITIGATIONEVENT._serialized_start=344
+  _MITIGATIONEVENT._serialized_end=377
+  _INSIGHTEVENT._serialized_start=379
+  _INSIGHTEVENT._serialized_end=409
+  _GETVIEWMODELREQUEST._serialized_start=412
+  _GETVIEWMODELREQUEST._serialized_end=688
+  _EVENTVIEWDETAIL._serialized_start=690
+  _EVENTVIEWDETAIL._serialized_end=813
+  _EVENTVIEWMODEL._serialized_start=816
+  _EVENTVIEWMODEL._serialized_end=1355
+  _GETVIEWMODELRESPONSE._serialized_start=1357
+  _GETVIEWMODELRESPONSE._serialized_end=1456
+  _EVENTVIEWMODELSERVICE._serialized_start=1968
+  _EVENTVIEWMODELSERVICE._serialized_end=2375
 # @@protoc_insertion_point(module_scope)
